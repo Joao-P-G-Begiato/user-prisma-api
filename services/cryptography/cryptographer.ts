bfe7b6011103277ee73a8dc9bf28e955f8d4d6f0
João Paulo Gotardi Begiato <jpbegiato@hotmail.com>
2023-09-14 15:36:12 -0300

    Creation of Feature: Cryptographer

---
+++ services/cryptography/cryptographer.ts
@@ -0,0 +1,13 @@
+const bcrypt = require ('bcrypt')
+
+class Cryptographer{
+    static async crypt(string : string): Promise<string>{
+        const crypt = await bcrypt.hash(string, 10)
+        return crypt
+    }
+
+    static async check(string : string , hash:string):Promise<boolean> {
+        const check = await bcrypt.compare(string, hash)
+        return check
+    }
+}
